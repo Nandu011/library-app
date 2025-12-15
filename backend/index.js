d7be0c4b3188235a52b5ee446157717aadec46e7
Nandu Narayanan <nandu.narayanan011@gmail.com>
2025-12-15 15:46:56 +0530

    Admin routes updated

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const PORT = 5000; // port for server to listen on
 const bookRoutes = require('./routes/bookRoutes'); // Book Routes
 const userRoute = require('./routes/userRoutes');
 const reviewRoutes = require('./routes/reviews');
+const adminRoutes = require('./routes/adminRoutes');
 require('./config/db')// connect to DB
 
 
@@ -16,6 +17,8 @@ app.use('/api/borrow', require('./routes/borrowRoutes'));
 app.get("/", (req, res) =>   res.send("Library app connected to PostgreSQL 🚀"));// Base Route
 app.use('/api/books', bookRoutes)// Book Routes
 app.use('/api/reviews', reviewRoutes);
+app.use('/api/admin', adminRoutes);
+
 
 
 // Start server
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -42,3 +42,56 @@ router.post("/users/add", verifyAdmin, async (req, res) => {
         res.status(500).json({ message: "Server error"});
     }
 });
+
+// Get all borrowed books
+router.get("/borrowed", verifyAdmin, async (req, res) => {
+    try {
+        const borrowed =  await pool.query(`
+            SELECT bb.id, u.name AS username, u.mobile,
+            b.title, c.unique_code,
+            bb.borrowed_date, bb.return_date, bb.returned
+            FROM borrowed_books bb
+            JOIN users u ON bb.user_id = u.id
+            JOIN book_copies c ON bb.copy_id = c.id
+            JOIN books b ON c.book_id= b.id
+            ORDER BY bb.borrowed_date DESC
+            `);
+
+            res.json(borrowed.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Server error"});
+    }
+});
+
+//Force return a book
+router.put("/force-return/:borrowId", verifyAdmin, async (req, res) => {
+    const {borrowId} = req.params;
+
+    try {
+        //Mark as returned
+        const updated = await pool.query(`
+            UPDATE borrowed_books
+            SET returned = TRUE, return_date = NOW()
+            WHERE id = $1 AND returned = FALSE
+            RETURNING copy_id`, 
+        [borrowId]
+    );
+
+    if (updated.rows.length === 0) 
+        return res.status(404).json({message: 'Record not found or already returned'});
+
+    //Mark copy as available
+    await pool.query(
+        `UPDATE book_copies SET is_borrrowed =  FALSE WHERE id = $1`,
+        [updated.rows[0].copy_id]
+    );
+
+    res.json({ message: "Book force-returned successfully"});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Server error"});
+    }
+});
+
+module.exports = router;
