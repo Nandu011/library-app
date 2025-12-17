4349380c5ae07246a52c5c2706673f0bbb6a24f8
Nandu Narayanan <nandu.narayanan011@gmail.com>
2025-12-17 17:01:42 +0530

    Updated adminRoutes

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -3,6 +3,58 @@ const pool = require("../config/db");
 const {verifyAdmin} = require('../middleware/authMiddleware');
 const router = express.Router();
 
+
+//Admin Dashboard stats
+router.get('/dashboard', verifyAdmin, async (req, res) => {
+    try {
+        const stats = await pool.query(`
+            SELECT
+            (SELECT COUNT(*) FROM books) AS total_books,
+            (SELECT COUNT(*) FROM book_copies) AS total_copies,
+            (SELECT COUNT(*) FROM book_copies WHERE is_available = true) AS available_copies,
+            (SELECT COUNT(*) FROM book_copies WHERE is_available = false) AS borrowed_copies,
+            (SELECT COUNT(*) FROM users) AS total_users,
+            (SELECT COUNT(*) FROM borrowed_books WHERE returned = false) AS active_borrows,
+            (
+            SELECT COUNT(*) FROM borrowed_books
+            WHERE returned = false AND due_date < CURRENT_DATE ) AS overdue_books`);
+
+            res.json(stats.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error'});
+    }
+});
+
+// Get overdue books (Admin)
+router.get('/overdue', verifyAdmin, async (req, res) => {
+    try {
+        const result = await pool.query(`
+            SELECT 
+            bb.id AS borrow_id,
+            u.name,
+            u.email,
+            u.mobile,
+            b.title,
+            bc.unique_code,
+            bb.borrow_date,
+            bb.due_date,
+            CURRENT_DATE - bb.due_date AS overdue_days
+            FROM borrowed_books bb
+            JOIN users u ON bb.user_id = u.id
+            JOIN book_copies bc ON bb.book_copy_id = bc.id
+            JOIN books b ON bc.book_id = b.id
+            WHERE bb.returned = false
+            AND bb.due_date < CURRENT_DATE
+            ORDER BY overdue_days DESC`);
+
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error'});
+    }
+});
+
 // Get all users
 router.get('/users', verifyAdmin, async (req, res) => {
     try {
